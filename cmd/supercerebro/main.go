db8d246eb048cf81f797b71a0f9ae823ec13d983
Matsu <124514952+Muramatsuu@users.noreply.github.com>
2025-08-02 14:47:02 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/supercerebro/main.go
@@ -0,0 +1,23 @@
+// cmd/supercerebro/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"supercerebro/internal/supercerebro"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := supercerebro.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
